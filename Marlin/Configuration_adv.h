0f0b7da9980aa1cca814d76027c6967d7b7b87d6
Duncan Band <band.duncan@gmail.com>
2021-01-02 19:04:22 +0000

    Microstepping to 16 with interpolation

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2266,7 +2266,7 @@
   #if AXIS_IS_TMC(X)
     #define X_CURRENT       1200        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  250  // (mA) RMS current for sensorless homing
-    #define X_MICROSTEPS     256    // 0..256
+    #define X_MICROSTEPS     16    // 0..256
     #define X_RSENSE          0.11
     #define X_CHAIN_POS      -1    // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
   #endif
@@ -2282,7 +2282,7 @@
   #if AXIS_IS_TMC(Y)
     #define Y_CURRENT       1200
     #define Y_CURRENT_HOME  250
-    #define Y_MICROSTEPS     256
+    #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
     #define Y_CHAIN_POS      -1
   #endif
@@ -2298,7 +2298,7 @@
   #if AXIS_IS_TMC(Z)
     #define Z_CURRENT       1000
     #define Z_CURRENT_HOME  250
-    #define Z_MICROSTEPS     256
+    #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
     #define Z_CHAIN_POS      -1
   #endif
@@ -2329,7 +2329,7 @@
 
   #if AXIS_IS_TMC(E0)
     #define E0_CURRENT      1200
-    #define E0_MICROSTEPS    256
+    #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
   #endif
